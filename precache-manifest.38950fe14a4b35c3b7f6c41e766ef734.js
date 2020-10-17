6db2c5d3e2dc98ec2cc8bfc505193f705674b91e
siddharthh98 <siddharthh.gaurr@gmail.com>
2020-10-17 22:59:27 +0530

    Updates

---
+++ precache-manifest.38950fe14a4b35c3b7f6c41e766ef734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb94fa631a37802bf9b1669d7f575e2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a162c7566d0e09fe4807",
+    "url": "/monsters-rolodex/static/css/main.15861d94.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a162c7566d0e09fe4807",
+    "url": "/monsters-rolodex/static/js/main.0ebd8f69.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
